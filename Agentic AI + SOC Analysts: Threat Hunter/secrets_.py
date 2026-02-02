e2bc2988f6adb4d05d93fedc65a4574ae5bcbe2b
Alexis McGuire <112157311+Chibiaiko@users.noreply.github.com>
2026-02-02 14:32:05 +0900

    Update secrets_.py with Azure CLI instructions
    Added comments for Azure CLI installation and API key setup.

--- Agentic AI + SOC Analysts: Threat Hunter/secrets_.py
+++ Agentic AI + SOC Analysts: Threat Hunter/secrets_.py
@@ -1,1 +1,7 @@
-
+# Install Azure CLI
+# Run: az login
+# 
+API_KEY = "_________" # Fund and put your openAI API key here or any API key currently using
+LOG_ANALYTICS_WORKSPACE_ID = "_________" # after joining the cyber range, put the Log Analytics Workspace ID here
+# https://portal.azure.com/#@lognpacific.com/resource/subscriptions/3c95e63a-895a-4386-991e-edbbf57de5c8/resourceGroups/Cyber-Range-Admin-SOC/providers/Microsoft.OperationalInsights/workspaces/LAW-Cyber-Range/Overview
+# This can be for any workplace that has Log Analytics with Azure
